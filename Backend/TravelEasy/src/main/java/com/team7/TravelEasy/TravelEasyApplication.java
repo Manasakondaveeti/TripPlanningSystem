a78fd3a3c921683782961e088723858181ea8eab
Manasa Kondaveeti <kondaveetimanasa77@gmail.com>
2024-05-29 15:59:06 -0400

    Initial scratch code

---
+++ Backend/TravelEasy/src/main/java/com/team7/TravelEasy/TravelEasyApplication.java
@@ -0,0 +1,13 @@
+package com.team7.TravelEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelEasyApplication.class, args);
+	}
+
+}
---
+++ Backend/TravelEasy/src/test/java/com/team7/TravelEasy/TravelEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.TravelEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
