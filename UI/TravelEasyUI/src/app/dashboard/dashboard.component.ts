76db7dac8e56cb76d9cfab6f8de08cf7061d1b67
Manasa Kondaveeti <kondaveetimanasa77@gmail.com>
2024-07-24 19:10:02 -0400

    add dashboard ig

--- UI/TravelEasyUI/src/app/dashboard/dashboard.component.ts
+++ UI/TravelEasyUI/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+ 
+  imports: [
+    CommonModule,
+    FormsModule,
+    
+  ],
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+
 })
 export class DashboardComponent {
 
