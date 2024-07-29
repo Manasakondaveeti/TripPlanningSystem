186a1e62fdf1a6151ef3b1de3b8800141c60d1ee
Jaya Sai Hrithik Padavala <71377534+HrithikPadavala@users.noreply.github.com>
2024-07-29 19:27:34 -0400

    Update FlightOrderRepository.java

--- Backend/TravelEasy/src/main/java/com/team7/TravelEasy/flightorders/FlightOrderRepository.java
+++ Backend/TravelEasy/src/main/java/com/team7/TravelEasy/flightorders/FlightOrderRepository.java
@@ -14,4 +14,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface FlightOrderRepository extends JpaRepository<FlightOrder, Long> {
+    FlightOrder findByUsername(String username);
 }
